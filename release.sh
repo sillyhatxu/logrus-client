cffbda40e1b0f584058ef7abcb923edd60cfd622
sillyhat <xushikuan1@163.com>
2019-09-08 14:26:50 +0800

    release logrus-client

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#git checkout -b go-mod-release-new-version v2.0.0
+#git tag -a -m "Tag version 2.0.0, go mod release" v2.0.0
 git add .
 git commit -m 'release logrus-client'
 git push origin master
