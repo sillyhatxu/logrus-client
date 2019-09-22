190ff1ea8cd9db96ff7817b35f0949b10a04c9d7
sillyhat <xushikuan1@163.com>
2019-09-22 23:19:31 +0800

    myself version v3.0.2

--- constants/constants.go
+++ constants/constants.go
@@ -11,21 +11,21 @@ var DefaultTextFormatter = &logrus.TextFormatter{
 	TimestampFormat:  string("2006-01-02 15:04:05"),
 	QuoteEmptyFields: true,
 	FieldMap: *&logrus.FieldMap{
-		logrus.FieldKeyMsg:   "message",
-		logrus.FieldKeyTime:  "@timestamp",
-		logrus.FieldKeyFunc:  "method",
-		logrus.FieldKeyFile:  "source",
-		logrus.FieldKeyLevel: "@level",
+		logrus.FieldKeyMsg:  "message",
+		logrus.FieldKeyTime: "@timestamp",
+		logrus.FieldKeyFunc: "method",
+		logrus.FieldKeyFile: "source",
+		//logrus.FieldKeyLevel: "@level",
 	},
 }
 
 var DefaultJSONFormatter = &logrus.JSONFormatter{
 	TimestampFormat: time.RFC3339Nano,
 	FieldMap: *&logrus.FieldMap{
-		logrus.FieldKeyMsg:   "message",
-		logrus.FieldKeyTime:  "@timestamp",
-		logrus.FieldKeyFunc:  "method",
-		logrus.FieldKeyFile:  "source",
-		logrus.FieldKeyLevel: "@level",
+		logrus.FieldKeyMsg:  "message",
+		logrus.FieldKeyTime: "@timestamp",
+		logrus.FieldKeyFunc: "method",
+		logrus.FieldKeyFile: "source",
+		//logrus.FieldKeyLevel: "@level",
 	},
 }
